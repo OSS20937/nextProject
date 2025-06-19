74910b6d6d01f31e315d4f08d1e1bcc4b5653454
OSS20937 <sangseop98@gmail.com>
2025-06-20 00:59:07 +0900

    apiTest

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,27 +1,25 @@
 'use client'
-import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function AboutPage() {
-  const [ping, setPing] = useState<string>('Loading...')
+  const [data, setData] = useState('')
 
   useEffect(() => {
-    axios
-      .get('https://72a8-211-33-223-91.ngrok-free.app/api/test/ping')
-      .then((res) => {
-        setPing(res.data)
-      })
-      .catch((error) => {
-        console.error('API 요청 실패:', error)
-        setPing('에러 발생')
-      })
+    axios.get('https://72a8-211-33-223-91.ngrok-free.app/api/test/ping', {
+      headers: {
+        'ngrok-skip-browser-warning': 'true'
+      }
+    })
+      .then(res => setData(res.data))
+      .catch(err => console.error(err))
   }, [])
 
   return (
     <main>
       <h1>About Page</h1>
       <p>This is the about page of my portfolio.</p>
-      <p>API 응답: {ping}</p>
+      <p><strong>API 응답:</strong> {data}</p>
     </main>
   )
 }
