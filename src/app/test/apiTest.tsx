0e25c58c78d177e3fdf682d3105b1c04f4f8ec83
oreo9 <sangseop98@gmail.com>
2025-06-20 18:13:10 +0900

    apiTest

---
+++ src/app/test/apiTest.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { API_BASE_URL } from '@/constans/api'
+
+
+export default function ApiTestPage() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/wizTechHrApi/test/ping`, {
+      method: 'GET',
+      headers: {
+        'ngrok-skip-browser-warning': 'true',
+      },
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error: ${res.status}`)
+        return res.json() // 또는 .json() - 응답 타입에 따라 다름
+      })
+      .then((data) => setMessage(data))
+      .catch((err) => setMessage(err.message))
+  }, [])
+
+  return (
+    <main>
+      <h1>API 테스트</h1>
+      <p><strong>응답:</strong> {message}</p>
+    </main>
+  )
+}
