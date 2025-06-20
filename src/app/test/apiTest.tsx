035d4c8a85044af708327e85618102a7830d6236
oreo9 <sangseop98@gmail.com>
2025-06-20 18:16:57 +0900

    apiTest

--- src/app/test/apiTest.tsx
+++ src/app/test/apiTest.tsx
@@ -1,24 +1,19 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { API_BASE_URL } from '@/constans/api'
-
 
 export default function ApiTestPage() {
   const [message, setMessage] = useState('')
 
   useEffect(() => {
-    fetch(`${API_BASE_URL}/wizTechHrApi/employee/hong@example.com`, {
-      method: 'GET',
+    axios.get(`${API_BASE_URL}/wizTechHrApi/employee/hong@example.com`, {
       headers: {
-        'ngrok-skip-browser-warning': 'true',
-      },
+        'ngrok-skip-browser-warning': 'true'
+      }
     })
-      .then((res) => {
-        if (!res.ok) throw new Error(`Error: ${res.status}`)
-        return res.json() // 또는 .json() - 응답 타입에 따라 다름
-      })
-      .then((data) => setMessage(data))
+      .then((res) => setMessage(JSON.stringify(res.data)))
       .catch((err) => setMessage(err.message))
   }, [])
 
